553b62087094452e8579e3500b32a743ab727a35
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-01-21 16:40:48 +0100

    added Models

---
+++ ToDoList/Models/ToDoList.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description")]
+        public string Description { get; set; } = "";
+
+        [Required(ErrorMessage = "Please enter a date")]
+        public DateTime? DueDate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a category")]
+        public string CategoryId { get; set; } = "";
+
+        [ValidateNever]
+        public string Category { get; set; } = null!;
+    }
+}
